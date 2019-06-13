f6967001302a1b6e12e307673c39f8351accd26f
erwin-oei <erwin.oei@kargo.tech>
2019-06-13 14:57:22 +0700

    add Guardian config

--- config/config.exs
+++ config/config.exs
@@ -25,6 +25,11 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Guardian config
+config :medium_graphql_api, MediumGraphqlApi.Guardian,
+       issuer: "medium_graphql_api",
+       secret_key: "4VGxtto3zJjwwgsKl/Ajw7EwUzF3pZ4O50Q7wzioTGwILpMCSzjEOAedkLCuT62O"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
