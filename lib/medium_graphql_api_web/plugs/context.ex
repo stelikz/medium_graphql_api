dc56d50992509ceb6201d8a940babbce353cf16e
erwin-oei <erwin.oei@kargo.tech>
2019-06-17 10:11:45 +0700

    add user to context plug

---
+++ lib/medium_graphql_api_web/plugs/context.ex
@@ -0,0 +1,25 @@
+defmodule MediumGraphqlApiWeb.Plugs.Context do
+  @behaviour Plug
+
+  import Plug.Conn
+
+  def init(opts) do
+    opts
+  end
+
+  def call(conn, _) do
+    context = build_context(conn)
+    Absinthe.Plug.put_options(conn, context: context)
+  end
+
+  defp build_context(conn) do
+    with ["Bearer " <> token] <- get_req_header(conn, "authorization"),
+    {:ok, claims} <- MediumGraphqlApi.Guardian.decode_and_verify(token),
+    {:ok, user} <- MediumGraphqlApi.Guardian.resource_from_claims(token) do
+     %{current_user: user}
+    else
+      _ -> %{}
+    end
+
+  end
+end
